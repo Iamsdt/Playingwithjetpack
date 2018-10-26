d727d1d5e0440abdf0ee9cbea47b984b60c5c5e0
Shudipto <Shudiptotrafder@gmail.com>
2018-10-26 12:16:00 +0600

    data binding

---
+++ app/src/main/java/com/iamsdt/playingwithjetpack/databinding/Note.kt
@@ -0,0 +1,8 @@
+package com.iamsdt.playingwithjetpack.databinding
+
+/**
+ * Created by Shudipto Trafder on 10/15/2018.
+ * at 9:03 PM
+ */
+
+class Note(val title: String, val des: String)
---
+++ app/src/main/java/com/iamsdt/playingwithjetpack/databinding/NoteAddActivity.kt
@@ -0,0 +1,23 @@
+package com.iamsdt.playingwithjetpack.databinding
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import com.iamsdt.playingwithjetpack.R
+
+import kotlinx.android.synthetic.main.activity_note_add.*
+
+class NoteAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
