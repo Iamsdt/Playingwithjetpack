aab6c55b1ee738ca40473c65c7c5e9d375b820be
Shudipto <Shudiptotrafder@gmail.com>
2018-12-03 22:11:46 +0600

    update
    Took 6 minutes

--- app/src/main/java/com/iamsdt/playingwithjetpack/databinding/NoteAddActivity.kt
+++ app/src/main/java/com/iamsdt/playingwithjetpack/databinding/NoteAddActivity.kt
@@ -2,7 +2,7 @@ package com.iamsdt.playingwithjetpack.databinding
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import com.iamsdt.playingwithjetpack.R
 
 import kotlinx.android.synthetic.main.activity_note_add.*
